703761bfeb76d507fba6bac191fb90ca3164820f
mamanzano <tesidhc@gmail.com>
2015-03-09 18:05:26 -0600

    templates

--- Sfotipy/sfotipy/artists/models.py
+++ Sfotipy/sfotipy/artists/models.py
@@ -5,3 +5,6 @@ class Artist(models.Model):
 	last_name = models.CharField(max_length=255, blank=True)
 	biography = models.TextField(blank=True)
 	
+
+	def __unicode__(self):
+		return self.first_name + ' ' + self.last_name
--- Sfotipy/sfotipy/tracks/views.py
+++ Sfotipy/sfotipy/tracks/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Track
 
-# Create your views here.
+def track_view(request, title):
+	# try:
+	# 	track = Track.objects.get(title=title)
+	# except Track.DoesNotExist:
+	# 	raise Http404
+	track = get_object_or_404(Track, title=title)
+	return render(request,'track.html'{'track': track})
